f6d96b6ddc19819a582200e2fc736d935eb7b2f4
eguneys <eguneys@gmail.com>
2026-03-01 05:48:04 +0300

    is_world

--- src/chess.pl
+++ src/chess.pl
@@ -45,12 +45,9 @@ select(Piece-From, Board, BFrom),
 exclude(_Captured-To, BFrom, BTo),
 BAfter = [Piece-To | BTo].
 
-add_world_from_to(W-B, From, To, W2B2) :- 
+add_world_from_to(W-B, From, To, W2-B2) :- 
 board_move(B, From, To, B2),
-W2B2 = _W2-B2,
-add_world(W, From-To, W2B2).
+add_world(W-B, From-To, B2, W2).
 
-
-root_world_with_legality(B) :- add_root_world(B).
 
 b_on_a1([white-bishop-(a-1)]).
